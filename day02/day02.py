d3385a775a0adfb28e1b7cabb5a62eede242bfd3
Geoffrey Mo <geoffreyklmo@gmail.com>
2023-12-04 19:56:32 -0500

    Update day 2 solution

--- day02/day02.py
+++ day02/day02.py
@@ -8,81 +8,35 @@ args = parser.parse_args()
 with open(args.filename) as f:
     lines = f.read().splitlines()
 
+colours = ['red', 'green', 'blue']
+
 # part 1
 possible = {'red': 12,
             'green': 13,
             'blue': 14,
            }
 allowed_games = []
+# part 2
+powers = []
 
 for line in lines:
     game_num = int(line.split(':')[0].split('Game ')[-1])
-    revealed = {'red': 0,
-                'green': 0,
-                'blue': 0,
-               }
-    reveals = line.split(': ')[-1].split(';')
-    for reveal in reveals:
-        this_reveal = {'red': 0,
-                    'green': 0,
-                    'blue': 0,
-                   }
-        revealed_colours = reveal.split(',')
-        for revealed_colour in revealed_colours:
-            this_colour = {}
-            for colour in ['red', 'green', 'blue']:
-                this_colour[colour] = (int(
-                    re.sub('[^0-9]', '', revealed_colour)) if colour in
-                    revealed_colour else 0)
-            for colour in ['red', 'green', 'blue']:
-                if this_colour[colour] > this_reveal[colour]:
-                    this_reveal[colour] = this_colour[colour]
-        # each this_reveal shows how many colours were in a particular reveal
-        # if for any colour, the colour from this_reveal is greater than the
-        # revealed colour, replace the revealed colour with this
-        for colour in ['red', 'green', 'blue']:
-            if this_reveal[colour] > revealed[colour]:
-                revealed[colour] = this_reveal[colour]
+    game_colours = {}
+    for col in colours:
+        col_instances = re.findall(rf'\d+ {col}', line)
+        col_max = max([
+            int(instance.split(' ')[0]) for instance in col_instances])
+        game_colours[col] = col_max
+
     # is this game allowed?
-    allowed = all([(revealed[colour] <= possible[colour])
+    allowed = all([(game_colours[colour] <= possible[colour])
                for colour in ['red', 'green', 'blue']])
     if allowed:
         allowed_games.append(game_num)
 
-print(f'part 1: sum of allowed game indices: {sum(allowed_games)}')
-
-# part 2
-powers = []
-for line in lines:
-    game_num = int(line.split(':')[0].split('Game ')[-1])
-    revealed = {'red': 0,
-                'green': 0,
-                'blue': 0,
-               }
-    reveals = line.split(': ')[-1].split(';')
-    for reveal in reveals:
-        this_reveal = {'red': 0,
-                    'green': 0,
-                    'blue': 0,
-                   }
-        revealed_colours = reveal.split(',')
-        for revealed_colour in revealed_colours:
-            this_colour = {}
-            for colour in ['red', 'green', 'blue']:
-                this_colour[colour] = (int(
-                    re.sub('[^0-9]', '', revealed_colour)) if colour in
-                    revealed_colour else 0)
-            for colour in ['red', 'green', 'blue']:
-                if this_colour[colour] > this_reveal[colour]:
-                    this_reveal[colour] = this_colour[colour]
-        # each this_reveal shows how many colours were in a particular reveal
-        # if for any colour, the colour from this_reveal is greater than the
-        # revealed colour, replace the revealed colour with this
-        for colour in ['red', 'green', 'blue']:
-            if this_reveal[colour] > revealed[colour]:
-                revealed[colour] = this_reveal[colour]
-    # power of this game
-    power = revealed['red'] * revealed['green'] * revealed['blue']
+    # part 2: power of these games
+    power = game_colours['red'] * game_colours['green'] * game_colours['blue']
     powers.append(power)
 
+print(f'part 1: sum of allowed game indices: {sum(allowed_games)}')
 print(f'part 2: sum of powers is {sum(powers)}')
